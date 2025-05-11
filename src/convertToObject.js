a64075556e6f85340f471bf0d3e7166cef97b457
Bohdan <takhaiev@icloud.com>
2025-05-11 23:16:16 +0300

    add task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,23 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  return sourceString
+    .trim()
+    .split(';')
+    .map((declaration) => declaration.trim())
+    .filter((declaration) => declaration)
+    .reduce((accumulator, declaration) => {
+      const parts = declaration.split(':');
+
+      if (parts.length === 2) {
+        const key = parts[0].trim();
+        const value = parts[1].trim();
+
+        accumulator[key] = value;
+      }
+
+      return accumulator;
+    }, {});
 }
 
 module.exports = convertToObject;
